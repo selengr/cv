114edac416eae72e45d18dc6462971cbe47ede99
yourusername <reza1997karbakhsh@gmail.com>
2023-01-25 14:29:40 +0330

    create Search component

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,16 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
+
   theme: {
     extend: {},
+    screens: {
+      xs: '375px',
+      sm: '600px',
+      md: '900px',
+      lg: '1200px',
+      xl: '1536px',
+    },
   },
   plugins: [],
 }
