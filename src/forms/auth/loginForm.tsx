cb00d6d89d27e142eae08281bcbad24b35dfea69
selengr <reza1997karbakhsh@gmail.com>
2026-08-24 21:35:07 +0330

    start updating whole progect and a modern rewrite - v3

--- src/forms/auth/loginForm.tsx
+++ src/forms/auth/loginForm.tsx
@@ -10,47 +10,44 @@ import callApi from "@/helpers/callApi";
 
 const phoneRegExp = /^(0|0098|\+98)9(0[1-5]|[13]\d|2[0-2]|98)\d{7}$/;
 
-const loginFormValidationSchema = yup.object({
-  phone: yup
-    .string()
-    .required("شماره موبایل الزامی است")
-    .min(8)
-    .matches(phoneRegExp, "فرمت شماره موبایل صحیح نیست"),
-});
-
 interface LoginFormProps {
   setToken: (token: string) => void;
+  router: ReturnType<typeof useRouter>;
 }
 
-function LoginFormWithRouter(
-  props: LoginFormProps & { router: ReturnType<typeof useRouter> },
-) {
-  const Form = withFormik<typeof props, LoginFormValuesInterface>({
-    mapPropsToValues: () => ({
-      phone: "",
-    }),
-    validationSchema: loginFormValidationSchema,
-    handleSubmit: async (values, { props: formProps, setFieldError }) => {
-      try {
-        const res = await callApi().post("/auth/login", values);
-        if (res.status === 200) {
-          formProps.setToken(res.data.token);
-          formProps.router.push("/auth/login/step-two");
-        }
-      } catch (error) {
-        if (error instanceof ValidationError) {
-          Object.entries(error.messages).forEach(([key, value]) =>
-            setFieldError(key, value),
-          );
-        }
+const FormikLoginForm = withFormik<LoginFormProps, LoginFormValuesInterface>({
+  mapPropsToValues: () => ({
+    phone: "",
+  }),
+  validationSchema: yup.object({
+    phone: yup
+      .string()
+      .required("شماره موبایل الزامی است")
+      .min(8)
+      .matches(phoneRegExp, "فرمت شماره موبایل صحیح نیست"),
+  }),
+  handleSubmit: async (values, { props, setFieldError }) => {
+    try {
+      const res = await callApi().post("/auth/login", values);
+      if (res.status === 200) {
+        props.setToken(res.data.token);
+        props.router.push("/auth/login/step-two");
       }
-    },
-  })(InnerLoginForm);
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        Object.entries(error.messages).forEach(([key, value]) =>
+          setFieldError(key, value),
+        );
+      }
+    }
+  },
+})(InnerLoginForm);
 
-  return <Form {...props} />;
-}
-
-export default function LoginForm(props: LoginFormProps) {
+export default function LoginForm({
+  setToken,
+}: {
+  setToken: (token: string) => void;
+}) {
   const router = useRouter();
-  return <LoginFormWithRouter {...props} router={router} />;
+  return <FormikLoginForm setToken={setToken} router={router} />;
 }
