796b02e1bb04108c6b80bc4694f344bb0292a143
selengr <reza1997karbakhsh@gmail.com>
2026-08-26 23:37:56 +0330

    add update user role api call

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,7 +1,12 @@
 import callApi from "@/helpers/callApi";
+import type { ShopRole } from "@/helpers/roles";
 import type { UserType } from "@/models/user";
 
 export async function GetUsers() {
   const res = await callApi().get("/users");
   return (res.data?.users ?? []) as Array<UserType & { phone?: string }>;
+}
+
+export async function UpdateUserRole(userId: number, role: ShopRole) {
+  return await callApi().post(`/users/${userId}/role`, { role });
 }
