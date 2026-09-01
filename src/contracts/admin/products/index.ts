8e9db858f959bb4c9b60a23220f12235fe0fcbb2
selengr <reza1997karbakhsh@gmail.com>
2026-09-01 19:44:06 +0330

    add english fields to product form contract

--- src/contracts/admin/products/index.ts
+++ src/contracts/admin/products/index.ts
@@ -1,8 +1,10 @@
 export interface CreateProductInterface {
   title: string;
+  title_en: string;
   category_id: string;
   price: number;
   description: string;
+  body_en: string;
   stock: number;
   emoji: string;
   image: string;
